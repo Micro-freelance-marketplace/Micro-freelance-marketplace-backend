77e5147535c163a06c5f39e1c41f544dbe3287f5
yarbework <yarbemohaz3517@gmail.com>
2026-04-01 10:27:15 +0300

    creating validation files and applying cors and helmet on app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 import express from "express";
+import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
+
+app.use(helmet());
+app.use(cors(
+    {
+        origin: '*', //we can add routes here at the production time
+        optionsSuccessStatus: 200
+        
+    }
+))
+
 app.use(express.json());
 
 app.get("/health", (req, res) => res.send("Server is running"));

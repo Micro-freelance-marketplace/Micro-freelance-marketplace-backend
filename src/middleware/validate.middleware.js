674bcac5758672e300f46ce834ab4857912d686d
yarbework <yarbemohaz3517@gmail.com>
2026-04-02 14:22:56 +0300

    creating application, auth, and gig validators

---
+++ src/middleware/validate.middleware.js
@@ -0,0 +1,13 @@
+import { validationResult } from "express-validator";
+import AppError from "../utils/AppError";
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const message = errors.array().map((el) => el.msg).join(', ');
+        return next(new AppError(message, 400));
+    };
+    next();
+};
+
+export default validate;

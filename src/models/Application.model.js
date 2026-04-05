560bbbf7e8cb063ba8a9d7616beef875788eb45a
stv7-0 <aestif.t701@gmail.com>
2026-04-05 20:36:36 +0300

    Update Application.model.js

--- src/models/Application.model.js
+++ src/models/Application.model.js
@@ -1,39 +1,40 @@
 import mongoose from "mongoose";
 
 const applicationSchema = new mongoose.Schema(
-  {
-    gigId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Gig",
-      required: true,
-    },
+  {
+    gigId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Gig",
+      required: true,
+    },
 
-    applicantId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+    applicantId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
 
-    coverLetter: {
-      type: String,
-      required: true,
-      trim: true,
-    },
+    coverLetter: {
+      type: String,
+      required: true,
+      trim: true,
+    },
 
-    status: {
-      type: String,
-      enum: ["pending", "accepted", "rejected"],
-      default: "pending",
-    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
 
-    appliedAt: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  { timestamps: true }
+    appliedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
 );
 
 const Application = mongoose.model("Application", applicationSchema);
+
 
 export default Application;
